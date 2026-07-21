3e2f623485e45bdf395f2d80c7a9c04f0e33b8c0
avtffhj-sys <avtffhj@gmail.com>
2026-07-21 19:51:23 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ PythonProject2/
 │       └── logo.png
 └── templates/
     └── index.html  (هنا نضع قالب الموقع)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
